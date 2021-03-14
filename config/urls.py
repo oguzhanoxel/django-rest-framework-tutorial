31666e7d25817e9d91606bd6fd9ca51931b96394
oguzhanoxel <oguzhanoksel@hotmail.com>
2021-03-14 17:34:04 +0300

    added snippets in apps folder

--- config/urls.py
+++ config/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('quickstart/', include('apps.quickstart.urls')),
+    path('snippets/', include('apps.snippets.urls')),
 ]
